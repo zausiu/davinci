f5cccefa6fe41bf46e6b95660028b7baf9406eaf
kamuszhou <zausiu@gmail.com>
2018-05-05 10:58:56 +0800

    removed brush

--- client/index.js
+++ client/index.js
@@ -12,11 +12,11 @@ function create_directed_graph(svg, graph) {
     var width = +svg.attr("width"),
         height = +svg.attr("height");
 
-    let parentWidth = d3.select('svg').node().parentNode.clientWidth;
-    let parentHeight = d3.select('svg').node().parentNode.clientHeight;
-	
     var svg = d3.select('svg')
-    .attr('width', parentWidth)
+    let parentWidth = svg.node().parentNode.clientWidth;
+    let parentHeight = svg.node().parentNode.clientHeight;
+	
+    svg.attr('width', parentWidth)
     .attr('height', parentHeight)
 
     // remove any previous graphs
@@ -38,6 +38,7 @@ function create_directed_graph(svg, graph) {
     gMain.call(zoom);
 
     function zoomed() {
+        // When a zoom event listener is invoked, d3.event is set to the current zoom event. 
         gDraw.attr('transform', d3.event.transform);
     }
 
@@ -96,19 +97,23 @@ function create_directed_graph(svg, graph) {
 
     var simulation = d3.forceSimulation()
         .force("link", d3.forceLink()
-                .id(function(d) { return d.id; })
+                .id(function(d) {
+                    // return 1;
+                    // console.log(d);
+                    return d.id; 
+                })
                 .distance(function(d) { 
                     return 30;
-                    //var dist = 20 / d.value;
-                    //console.log('dist:', dist);
+                    var dist = 20 / d.value;
+                    console.log('dist:', dist);
 
                     return dist; 
                 })
               )
         .force("charge", d3.forceManyBody())
         .force("center", d3.forceCenter(parentWidth / 2, parentHeight / 2))
-        .force("x", d3.forceX(parentWidth/2))
-        .force("y", d3.forceY(parentHeight/2));
+        //.force("x", d3.forceX(parentWidth/2))
+        //.force("y", d3.forceY(parentHeight/2));
 
     simulation
         .nodes(graph.nodes)
@@ -129,102 +134,10 @@ function create_directed_graph(svg, graph) {
             .attr("cy", function(d) { return d.y; });
     }
 
-    var brushMode = false;
-    var brushing = false;
-
-    var brush = d3.brush()
-        .on("start", brushstarted)
-        .on("brush", brushed)
-        .on("end", brushended);
-
-    function brushstarted() {
-        // keep track of whether we're actively brushing so that we
-        // don't remove the brush on keyup in the middle of a selection
-        brushing = true;
-
-        node.each(function(d) { 
-            d.previouslySelected = shiftKey && d.selected; 
-        });
-    }
-
-    rect.on('click', () => {
-        node.each(function(d) {
-            d.selected = false;
-            d.previouslySelected = false;
-        });
-        node.classed("selected", false);
-    });
-
-    function brushed() {
-        if (!d3.event.sourceEvent) return;
-        if (!d3.event.selection) return;
-
-        var extent = d3.event.selection;
-
-        node.classed("selected", function(d) {
-            return d.selected = d.previouslySelected ^
-            (extent[0][0] <= d.x && d.x < extent[1][0]
-             && extent[0][1] <= d.y && d.y < extent[1][1]);
-        });
-    }
-
-    function brushended() {
-        if (!d3.event.sourceEvent) return;
-        if (!d3.event.selection) return;
-        if (!gBrush) return;
-
-        gBrush.call(brush.move, null);
-
-        if (!brushMode) {
-            // the shift key has been release before we ended our brushing
-            gBrush.remove();
-            gBrush = null;
-        }
-
-        brushing = false;
-    }
-
-    d3.select('body').on('keydown', keydown);
-    d3.select('body').on('keyup', keyup);
-
-    var shiftKey;
-
-    function keydown() {
-        shiftKey = d3.event.shiftKey;
-
-        if (shiftKey) {
-            // if we already have a brush, don't do anything
-            if (gBrush)
-                return;
-
-            brushMode = true;
-
-            if (!gBrush) {
-                gBrush = gBrushHolder.append('g');
-                gBrush.call(brush);
-            }
-        }
-    }
-
-    function keyup() {
-        shiftKey = false;
-        brushMode = false;
-
-        if (!gBrush)
-            return;
-
-        if (!brushing) {
-            // only remove the brush if we're not actively brushing
-            // otherwise it'll be removed when the brushing ends
-            gBrush.remove();
-            gBrush = null;
-        }
-    }
-
     function dragstarted(d) {
       if (!d3.event.active) simulation.alphaTarget(0.9).restart();
 
-        if (!d.selected && !shiftKey) {
+        if (!d.selected) {
             // if this node isn't selected, then we have to unselect every other node
             node.classed("selected", function(p) { return p.selected =  p.previouslySelected = false; });
         }
